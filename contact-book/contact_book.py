6656ecfb3d26c9a826f18b102b0b7c5fa3dbb337
Jessica <jban@sdf.org>
2020-08-16 12:38:39 +0300

    Fix logic in core

--- contact-book/contact_book.py
+++ contact-book/contact_book.py
@@ -3,59 +3,65 @@ from view import menu_prompt, start_menu_prompt
 
 
 if __name__ == '__main__':
-    selection = start_menu_prompt()
+    while True:
+        selection = start_menu_prompt()
 
-    session = db_connect()
+        session = db_connect()
 
-    # Add a new contact
-    if selection.startswith("Add"):
-        while True:
-            add_contact()
-            selection = menu_prompt("Add")
-            if selection.startswith("Return"):
-                pass
-                break
-            else:
-                continue
+        # Add a new contact
+        if selection.startswith("Add"):
+            while True:
+                add_contact()
+                selection = menu_prompt("Add")
+                if selection.startswith("Return"):
+                    break
+                elif selection.startswith("Exit"):
+                    exit()
+                else:
+                    continue
 
-    # Delete a contact
-    elif selection.startswith("Delete"):
-        while True:
-            delete_contact()
-            selection = menu_prompt("Delete")
-            if selection.startswith("Return"):
-                pass
-                break
-            else:
-                continue
+        # Delete a contact
+        elif selection.startswith("Delete"):
+            while True:
+                delete_contact()
+                selection = menu_prompt("Delete")
+                if selection.startswith("Return"):
+                    break
+                elif selection.startswith("Exit"):
+                    exit()
+                else:
+                    continue
 
-    # Search the contact book
-    elif selection.startswith("Search"):
-        while True:
-            search_results()
-            selection = menu_prompt("Search for")
-            if selection.startswith("Return"):
-                pass
-                break
-            else:
-                continue
+        # Search the contact book
+        elif selection.startswith("Search"):
+            while True:
+                search_results()
+                selection = menu_prompt("Search for")
+                if selection.startswith("Return"):
+                    break
+                elif selection.startswith("Exit"):
+                    exit()
+                else:
+                    continue
 
 
-    # Update contact information
-    elif selection.startswith("Update"):
-        while True:
-            update_contact()
-            selection = menu_prompt("Update")
-            if selection.startswith("Return"):
-                pass
-                break
-            else:
-                continue
+        # Update contact information
+        elif selection.startswith("Update"):
+            while True:
+                update_contact()
+                selection = menu_prompt("Update")
+                if selection.startswith("Return"):
+                    break
+                elif selection.startswith("Exit"):
+                    exit()
+                else:
+                    continue
 
-    # View all entries
-    elif selection.startswith("View"):
-        view_all_entries()
+        # View all entries
+        elif selection.startswith("View"):
+            view_all_entries()
 
-    # Exit the program.
-    elif selection.startswith("Exit"):
-        exit()
+        # Exit the program.
+        elif selection.startswith("Exit"):
+            exit()
+            break
