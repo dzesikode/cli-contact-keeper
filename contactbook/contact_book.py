973b33074da66fe9967612be20d0d1212a1cb3b4
Jessica <jban@sdf.org>
2020-08-21 20:17:55 +0300

    Specify imports

--- contactbook/contact_book.py
+++ contactbook/contact_book.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from helpers import *
+from helpers import db_connect, add_contact, delete_contact, search_results, \
+                    update_contact, view_all_entries
 from view import menu_prompt, start_menu_prompt
 
 
